9c46a08bb351d8e39eb6a0b6f1428ffed68d2f78
Sebyanin <feozru@yahoo.com>
2026-08-02 18:08:22 +0500

    Правка линков вивальди

--- modules/home/vivaldi.nix
+++ modules/home/vivaldi.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
   programs.vivaldi = {
@@ -13,4 +13,14 @@
 
     nativeMessagingHosts = [ pkgs.keepassxc ];
   };
+
+  # Home Manager теперь линкует NativeMessagingHosts recursively (каталог + json),
+  # а раньше это была одна симлинка на директорию в nix store. Старая симлинка
+  # блокирует mkdir при активации («Файл существует»).
+  home.activation.vivaldiNativeMessagingHosts = lib.hm.dag.entryBefore [ "checkLinkTargets" ] ''
+    nmh="$HOME/.config/vivaldi/NativeMessagingHosts"
+    if [ -L "$nmh" ]; then
+      $DRY_RUN_CMD rm -f "$nmh"
+    fi
+  '';
 }
